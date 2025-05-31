9afc59862903cb3b399fdfd3e34964052628137b
Henrique Soares <62728244+henriquesph@users.noreply.github.com>
2025-05-31 15:56:03 -0300

    matching arguments

--- Teste Unitários/Testando Conta/Banco/ContaTeste.Mock/ContaTeste.cs
+++ Teste Unitários/Testando Conta/Banco/ContaTeste.Mock/ContaTeste.cs
@@ -14,8 +14,13 @@ namespace ContaTeste.Mock
             var mock = new Mock<IValidadorCredito>();
 
             // Injeção de dependência
-            mock.Setup(x => x.ValidarCredito("0001", 5000)).Returns(true); // o método não foi criado em uma classe, quando tiver essas entradas a saída tem que true
+            //mock.Setup(x => x.ValidarCredito("0001", 5000)).Returns(true); o método não foi criado em uma classe, quando tiver essas entradas a saída tem que true
+
+            mock.Setup(x => x.ValidarCredito(It.IsAny<string>(), It.Is<decimal>(i => i <= 5000))).Returns(true); // it.any - aceita qualquer valor do tipo genérico passado e passa condição por Lambda
+
             conta.SetValidadorCredito(mock.Object); // Object é um atributo em que o objeto vai ser trabalhado, ele implementa a interface não o objeto Mock em si - não precisamos implementar os métodos da interface, para focar no método que vc quer testar - Evita a criação de classes p/ implementação (projeto enxuto)
+
+
             int resultadoEsperado = 5100;
 
             conta.SolicitarEmprestimo(5000);
